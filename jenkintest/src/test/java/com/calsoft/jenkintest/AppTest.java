7e68852ee6fd876032c76f438cbc1d38b5134f9e
digantasahoo <mail2digant@gmail.com>
2013-05-13 16:49:17 +0530

    jenkin modified

--- jenkintest/src/test/java/com/calsoft/jenkintest/AppTest.java
+++ jenkintest/src/test/java/com/calsoft/jenkintest/AppTest.java
@@ -9,7 +9,7 @@ import org.junit.Assert;
 public class AppTest {
 	App app =new App();
 	public void getSumTest(){
-		//System.out.println("hello testing....");
+		System.out.println("hello testing....");
 		Assert.assertEquals(15,app.getSum(10,5));
 	}
 }
