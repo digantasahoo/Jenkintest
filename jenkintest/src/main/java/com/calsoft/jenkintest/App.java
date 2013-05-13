10f94c7231a43f3c7154d98ac43bf2535ec97c44
digantasahoo <mail2digant@gmail.com>
2013-05-13 13:53:08 +0530

    jenkin.....

---
+++ jenkintest/src/main/java/com/calsoft/jenkintest/App.java
@@ -0,0 +1,16 @@
+package com.calsoft.jenkintest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int getSum(int num1,int num2){
+    	return num1+num2;
+    }
+}
---
+++ jenkintest/src/test/java/com/calsoft/jenkintest/AppTest.java
@@ -0,0 +1,14 @@
+package com.calsoft.jenkintest;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	App app =new App();
+	public void getSumTest(){
+		//Assert.assertEquals(15,app.getSum(10,5));
+	}
+}
