9bd22e51313b9ce37bd46d9cbe26b684f5eca657
digantasahoo <mail2digant@gmail.com>
2013-05-13 14:10:34 +0530

    jenkins job test

--- jenkintest/src/test/java/com/calsoft/jenkintest/AppTest.java
+++ jenkintest/src/test/java/com/calsoft/jenkintest/AppTest.java
@@ -9,7 +9,6 @@ import org.junit.Assert;
 public class AppTest {
 	App app =new App();
 	public void getSumTest(){
-		System.out.println("hello i am testing");
 		Assert.assertEquals(15,app.getSum(10,5));
 	}
 }
